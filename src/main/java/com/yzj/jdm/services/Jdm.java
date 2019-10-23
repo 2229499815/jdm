ae7f6f57a791626a27884502fe8639cc8f9a8ace
林真烜 <18950377474@163.com>
2019-10-23 23:52:09 +0800

    关于引用型参数无法返回的问题注释，以及替代接口的说明

--- src/main/java/com/yzj/jdm/services/Jdm.java
+++ src/main/java/com/yzj/jdm/services/Jdm.java
@@ -15,8 +15,13 @@ public class Jdm {
         Idmsoft idmsoft=new Idmsoft("dm.dmsoft");
         String ver = idmsoft.Ver();
         System.out.println(ver);
-
-
+        //很人多反应说 FindPic  引用类型的值无法返回，那就是真的无法返回，
+        // 但是dm提供了个FindPicEx是专门针对像java这种用不了指针类型的语言使用的
+        //所以当你遇到用不了的接口的时候你应该都能找到一个Ex的增强型接口，使用它就行了
+        String s = idmsoft.GetColor(100, 100);
+        System.out.println(s);
+        String str = idmsoft.FindPicEx(0, 0, 2000, 2000, "D:\\1.bmp", "000000", 0.8, 0);
+        System.out.println(str);
 
     }
 
